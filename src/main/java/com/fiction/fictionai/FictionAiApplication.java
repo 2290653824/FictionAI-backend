eb05c71205566d7deef43e86fadd5e2864ea2c52
zhengjian <2290653824@qq.com>
2025-02-09 10:40:46 +0800

    init project

---
+++ src/main/java/com/fiction/fictionai/FictionAiApplication.java
@@ -0,0 +1,13 @@
+package com.fiction.fictionai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FictionAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FictionAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiction/fictionai/FictionAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiction.fictionai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FictionAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
